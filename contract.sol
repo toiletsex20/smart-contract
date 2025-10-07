0858ba19dce12eb30bdd3a0bdb7f6ca456b4df0a
toiletsex20 <toiletsex20@gmail.com>
2025-10-06 21:40:06 -0400

    Create contract.sol

---
+++ contract.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT 
+// Repo: https://github.com/yourname/smart-contract
+// Deployer: 0xf465f21B80e15138996665bB285c71dc2b2B6b1d 
+pragma solidity ^0.8.20; 
+
+contract MyContract 
+{ // simple contract for demonstration
+}
