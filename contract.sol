40d8bc3db10c738d1c9fbfb05157f06f3ab39d12
toiletsex20 <toiletsex20@gmail.com>
2025-10-06 21:45:45 -0400

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT 
-// Repo: https://github.com/yourname/smart-contract
+// Repo: https://github.com/toiletsex20/smart-contract
 // Deployer: 0xf465f21B80e15138996665bB285c71dc2b2B6b1d 
 pragma solidity ^0.8.20; 
 
